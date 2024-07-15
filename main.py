961317dfa13db9b37ed7528ce4aa93c484cd13d9
Ritu <147260911+Ritu073@users.noreply.github.com>
2024-07-15 13:02:37 +0530

    Add files via upload
    Guess Game

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
 import random
-'''
-1 for snake
--1 for water 
-0 for gun
-'''
-computer = random.choice([-1, 0, 1])
-youstr = input("Enter your choice: ")
-youDict = {"s": 1, "w": -1, "g": 0}
-reverseDict = {1: "Snake", -1: "Water", 0: "Gun"}
+n = random.randint(1, 100) 
+a = -1
+guesses = 1
+while(a != n):
+    a = int(input("Guess the number: ")) 
+    if(a >n):
+        print("Lower number please")
+        guesses +=1
+    elif(a<n):
+        print("Higher number Please")
+        guesses +=1
 
-you = youDict[youstr]
-
-# By now we have 2 numbers (variables), you and computer
-
-print(f"You chose {reverseDict[you]}\nComputer chose {reverseDict[computer]}")
-
-if(computer == you):
-    print("Its a draw")
-
-else:
-    if(computer ==-1 and you == 1): 
-        print("You win!")
-
-    elif(computer ==-1 and you == 0):
-        print("You Lose!")
-
-    elif(computer == 1 and you == -1):
-        print("You lose!")
-
-    elif(computer ==1 and you == 0):
-        print("You Win!")
-
-    elif(computer ==0 and you == -1):
-        print("You Win!")
-
-    elif(computer == 0 and you == 1):
-        print("You Lose!")
-
-    else:
-        print("Something went wrong!")
+print(f"You have guessed the number {n} correctly in {guesses} attempts")
